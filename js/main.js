81128a76e1196fdb36c25598716067810428d091
mostafaashraf204 <mostafaashraf3100@gmaill.com>
2023-03-07 18:19:55 +0200

    new section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-var preloader = document.getElementById("preloader");
-window.addEventListener("load", () => {
-    preloader.style.display = "none";
-}, 5000)
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
